2274d49c4a7ed22fbf41893c7e83a1d7e8a39c46
Aleksey Antokhin <antokhin.aleksey@gmail.com>
2020-12-08 17:11:00 +0300

    first commit

---
+++ Green corner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Green corner
+//
+//  Created by Aleksey Antokhin on 08.12.2020.
+//  Copyright © 2020 Aleksey Antokhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
