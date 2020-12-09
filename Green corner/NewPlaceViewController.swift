eca6b648f81154fbf605d2a5a6154d4874e1177d
Aleksey Antokhin <antokhin.aleksey@gmail.com>
2020-12-09 15:36:55 +0300

    Image Selection

--- Green corner/NewPlaceViewController.swift
+++ Green corner/NewPlaceViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,9 +19,26 @@ class NewPlaceViewController: UITableViewController {
     }
     
     // MARK: Table view delegate
-    // скрываем клавиатуру если tup в влюбом месте кроме textField
+    // скрываем клавиатуру (если tup в влюбом месте кроме textField) вызываем метод chooseImagePicker
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+           let actionSheet = UIAlertController(title: nil,
+                                               message: nil,
+                                               preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true) // вызываем контроллер actionSheet
             
         } else {
             view.endEditing(true)
@@ -37,6 +56,29 @@ class NewPlaceViewController: UITableViewController {
             return true
         }
     }
+     
+// MARK: Work with image
+
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) { // выбираем источник изображения
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+           let imagePicker = UIImagePickerController() // создаем экземпляр класса UIImagePickerController
+            imagePicker.delegate = self // делигируем экземпляру класса NewPlaceViewController управление imagePicker
+            imagePicker.allowsEditing = true // позволит редактировать выбанное изображение
+            imagePicker.sourceType = source // определяем источник изображения // imagePicker - это контроллер
+            present(imagePicker, animated: true) // показываем(вызов в ) imagePicker контроллер
+            
+        }
         
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage // берем и присваиваем в imageOfPlace.image, значение по конкретному ключу info. Св-ва данной ст-ры определяют тип контента.
+        imageOfPlace.contentMode = .scaleAspectFit // масштабируем изображение по содержимому UIImage
+        imageOfPlace.clipsToBounds = true // обрезка по границе
+        dismiss(animated: true) // закрываем imagePickerController
+    }
+    
+}
     
 
