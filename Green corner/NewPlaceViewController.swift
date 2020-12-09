19a29b75b4d45a0596043c89b4cee7ab9fb04975
Aleksey Antokhin <antokhin.aleksey@gmail.com>
2020-12-09 15:59:52 +0300

    Action Sheet Customization

--- Green corner/NewPlaceViewController.swift
+++ Green corner/NewPlaceViewController.swift
@@ -22,16 +22,26 @@ class NewPlaceViewController: UITableViewController {
     // скрываем клавиатуру (если tup в влюбом месте кроме textField) вызываем метод chooseImagePicker
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
-           let actionSheet = UIAlertController(title: nil,
+           
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
+            
+            let actionSheet = UIAlertController(title: nil,
                                                message: nil,
                                                preferredStyle: .actionSheet)
             
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
                 self.chooseImagePicker(source: .camera)
             }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.chooseImagePicker(source: .photoLibrary)
             }
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             
             actionSheet.addAction(camera)
