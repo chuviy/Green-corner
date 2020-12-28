3933e66737197d2bcd0b332cb588e6b9313d66e5
Aleksey Antokhin <antokhin.aleksey@gmail.com>
2020-12-28 15:04:14 +0300

    Work with Annotations

--- Green corner/MapViewController.swift
+++ Green corner/MapViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    var place: Place!
+    let annotationIdentifire = "annotationIdentifire"
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // назначаем делегатом сам класс
+        mapView.delegate = self
+        // показываем точки на карте
+        setupPlaceMark()
 
     }
     
@@ -20,4 +30,66 @@ class MapViewController: UIViewController {
         dismiss(animated: true)
     }
     
+    private func setupPlaceMark() {
+        
+        guard let location = place.location else { return }
+        
+        // преобразуем координаы долготы и широты в пользовательский формат: город, улица, дом и т.д.
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error)  in
+        
+            if let error = error {
+                print("error: \(error)")
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            // получакем метку на карте
+            let placemark = placemarks.first
+            // описываем точку на карте
+            let annotation =  MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placeMarkLocation = placemark?.location else { return }
+            // привязываем аннотацию к точке на карте
+            annotation.coordinate = placeMarkLocation.coordinate
+            // показываем массив анотаций на карте
+            self.mapView.showAnnotations([annotation], animated: true)
+            // выделяем анотацию
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
+        
+    }
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    // отображение аннотации
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifire) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifire)
+            // Отображаем аннотацию в виде банера
+            annotationView?.canShowCallout = true
+        }
+        
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        
+        
+        return annotationView
+    }
+     
 }
--- Green corner/NewPlaceViewController.swift
+++ Green corner/NewPlaceViewController.swift
@@ -73,6 +73,18 @@ class NewPlaceViewController: UITableViewController {
             view.endEditing(true)
         }
     }
+    
+    // MARK: Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "showMap" {
+            return
+        }
+        
+        let mapVC = segue.destination as! MapViewController
+        mapVC.place = currentPlace
+    }
+    
     func savePlace() { // сохраняем данные о местах в БД
                 
         var image: UIImage?
